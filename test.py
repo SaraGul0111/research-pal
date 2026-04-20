f1c54230e9bfb231990a49e1185edd49fe312516
Sara Gul <saragulofficial@gmail.com>
2026-04-21 01:22:21 +0500

    remove unnecessary files

--- test.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyC_W0vPWGGQNGsdQhIhkB7HDxyyBf9Yf0I")
-
-for m in genai.list_models():
-    if "generateContent" in m.supported_generation_methods:
-        print(m.name)
